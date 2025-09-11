fb4e1a2d35e20ab718576aa35e024c31c015fb60
Shan <shansung25@gmail.com>
2025-09-11 10:36:59 +0800

    完成！！

--- app/src/main/java/com/example/thelastone/MainActivity.kt
+++ app/src/main/java/com/example/thelastone/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.thelastone
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.core.view.WindowCompat
 import com.example.thelastone.ui.navigation.AppScaffold
 import com.example.thelastone.ui.theme.AppTheme
@@ -15,7 +16,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false) // Edge-to-edge
         setContent {
-            AppTheme {
+            AppTheme( // ← 你自己的
+                darkTheme = isSystemInDarkTheme(), // 預設跟隨系統
+                dynamicColor = true                 // Android 12+ 啟用動態色
+            ) {
+                // 這裡放整個 App UI
                 AppScaffold()
             }
         }
--- app/src/main/java/com/example/thelastone/ui/theme/Theme.kt
+++ app/src/main/java/com/example/thelastone/ui/theme/Theme.kt
@@ -275,4 +275,3 @@ fun AppTheme(
         content = content
     )
 }
-
